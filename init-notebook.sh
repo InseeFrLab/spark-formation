f20b24fc22a8e78202a6d4637e092a1a22cd8244
avouacr <avouacr@pm.me>
2021-11-04 15:54:54 +0000

    put each notebook in a folder to isolate chapters

--- init-notebook.sh
+++ init-notebook.sh
@@ -9,4 +9,4 @@ chown -R jovyan:users /home/jovyan/work/spark-formation
 [ -f /home/jovyan/work/requirements.txt ] && pip install -r /home/jovyan/work/requirements.txt
 
 # Open a given notebook
-[ -z "$IPYNB_PATH" ] || echo "c.LabApp.default_url = '/lab/tree/spark-formation/${IPYNB_PATH}'" >> /home/jovyan/.jupyter/jupyter_server_config.py
+[ -z "$IPYNB_PATH" ] || echo "c.LabApp.default_url = '/lab/tree/${IPYNB_PATH%.*}/${IPYNB_PATH}'" >> /home/jovyan/.jupyter/jupyter_server_config.py
