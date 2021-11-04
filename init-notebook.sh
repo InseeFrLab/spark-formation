99787cd57c8d1c87bc6a7f824ed6f72702f8cd3b
avouacr <avouacr@pm.me>
2021-11-04 16:45:56 +0000

    fix dir spark

--- init-notebook.sh
+++ init-notebook.sh
@@ -2,11 +2,12 @@
 
 # Clone the repository in /home/jovyan/work
 REPO_URL=https://github.com/InseeFrLab/spark-formation.git
-git clone $REPO_URL /home/jovyan/work/spark-formation
-chown -R jovyan:users /home/jovyan/work/spark-formation
+DIR=spark-formation
+git clone $REPO_URL /home/jovyan/work/$DIR
+chown -R jovyan:users /home/jovyan/work/$DIR
 
 # Install dependencies
 [ -f /home/jovyan/work/requirements.txt ] && pip install -r /home/jovyan/work/requirements.txt
 
 # Open a given notebook
-[ -z "$IPYNB_PATH" ] || echo "c.LabApp.default_url = '/lab/tree/${IPYNB_PATH%.*}/${IPYNB_PATH}'" >> /home/jovyan/.jupyter/jupyter_server_config.py
+[ -z "$IPYNB_PATH" ] || echo "c.LabApp.default_url = '/lab/tree/${DIR}/${IPYNB_PATH%.*}/${IPYNB_PATH}'" >> /home/jovyan/.jupyter/jupyter_server_config.py
