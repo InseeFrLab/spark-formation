4a98133528bc16f6870851ee2fbee4c28d15bf0a
Comte <frederic.comte@insee.fr>
2021-12-03 16:50:10 +0100

    Update init-notebook.sh

--- init-notebook.sh
+++ init-notebook.sh
@@ -10,4 +10,4 @@ chown -R jovyan:users $ROOT_PROJECT_DIRECTORY/${REPO}
 [ -f /home/jovyan/work/requirements.txt ] && pip install -r /home/jovyan/work/requirements.txt
 
 # Open a given notebook when starting Jupyter Lab
-[ -z "$IPYNB_PATH" ] || echo "c.LabApp.default_url = '/lab/tree/${REPO}/${IPYNB_PATH%.*}/${IPYNB_PATH}'" >> /home/jovyan/.jupyter/jupyter_server_config.py
+[ -z "$IPYNB_PATH" ] || echo "c.LabApp.default_url = '/lab/tree/${REPO}/${IPYNB_PATH%.*}/${IPYNB_PATH}'" >> $HOME/.jupyter/jupyter_server_config.py
