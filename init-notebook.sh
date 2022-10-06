7273ad8e1fa8eabf6afedbc8910940839c43c82f
Comte <frederic.comte@insee.fr>
2022-10-06 21:40:49 +0200

    Update init-notebook.sh

--- init-notebook.sh
+++ init-notebook.sh
@@ -4,10 +4,12 @@
 REPO=formation-spark
 REPO_URL=https://github.com/InseeFrLab/${REPO}.git
 git clone $REPO_URL $ROOT_PROJECT_DIRECTORY/${REPO}
-chown -R jovyan:users $ROOT_PROJECT_DIRECTORY/${REPO}
+chown -R onyxia:users $ROOT_PROJECT_DIRECTORY/${REPO}
 
 # Install dependencies
-[ -f /home/jovyan/work/requirements.txt ] && pip install -r /home/jovyan/work/requirements.txt
+[ -f $HOME/work/requirements.txt ] && pip install -r $HOME/work/requirements.txt
 
-# Open a given notebook when starting Jupyter Lab
-[ -z "$IPYNB_PATH" ] || echo "c.LabApp.default_url = '/lab/tree/${REPO}/${IPYNB_PATH%.*}/${IPYNB_PATH}'" >> $HOME/.jupyter/jupyter_server_config.py
+# Open the relevant notebook when starting Jupyter Lab
+jupyter server --generate-config
+echo "c.LabApp.default_url = '/lab/tree/${IPYNB_PATH%.*}/${IPYNB_PATH}'" >> $HOME/.jupyter/jupyter_server_config.py
+
