0c5c38be9bca91f07f6de3c3d387429cb21d29aa
Comte <frederic.comte@insee.fr>
2022-10-10 12:08:07 +0200

    Update init-notebook.sh

--- init-notebook.sh
+++ init-notebook.sh
@@ -7,7 +7,7 @@ git clone $REPO_URL $ROOT_PROJECT_DIRECTORY/${REPO}
 chown -R onyxia:users $ROOT_PROJECT_DIRECTORY/${REPO}
 
 # Install dependencies
-[ -f $HOME/work/requirements.txt ] && pip install -r $HOME/work/requirements.txt
+[ -f $HOME/work/formation-spark/requirements.txt ] && pip install -r $HOME/work/formation-spark/requirements.txt
 
 # Open the relevant notebook when starting Jupyter Lab
 jupyter server --generate-config
