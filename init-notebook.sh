ea72b572b68aab3525f40a834f1bae7651ca50d6
fcomte <frederic.comte@insee.fr>
2024-06-12 15:25:04 +0000

    init-notebook.sh

--- init-notebook.sh
+++ init-notebook.sh
@@ -11,6 +11,6 @@ chown -R onyxia:users $ROOT_PROJECT_DIRECTORY/${REPO}
 
 # Open the relevant notebook when starting Jupyter Lab
 # jupyter server --generate-config
-echo "c.LabApp.default_url = '/lab/tree/formation-spark/${IPYNB_PATH%.*}/${IPYNB_PATH}'" >> $HOME/.jupyter/jupyter_server_config.py
+echo "c.LabApp.default_url = '/lab/tree/formation-spark/$1/$2'" >> $HOME/.jupyter/jupyter_server_config.py
 echo "c.IPKernelApp.capture_fd_output = False" >> $HOME/.jupyter/jupyter_server_config.py
 
